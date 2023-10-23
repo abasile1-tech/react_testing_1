a2c64a927d710f901fa856c00b14883b5cacee01
John <ashton.basile@gmail.com>
2023-10-23 14:54:03 +0100

    fixing the Counter file to use date-testid instead of id

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,23 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Counter = () => {
-  const [counter, setCounter] = React.useState(0)
+  const [counter, setCounter] = React.useState(0);
 
   const incrementCounter = () => {
     setCounter(counter + 1);
-  }
+  };
 
   const decrementCounter = () => {
     setCounter(counter - 1);
-  }
+  };
 
   return (
     <>
-      <h1 id="counter">{counter}</h1>
-      <button id="button-up" onClick={incrementCounter}> Up</button>
-      <button id="button-down" onClick={decrementCounter}>Down</button>
+      <h1 data-testid="counter">{counter}</h1>
+      <button data-testid="button-up" onClick={incrementCounter}>
+        {" "}
+        Up
+      </button>
+      <button data-testid="button-down" onClick={decrementCounter}>
+        Down
+      </button>
     </>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
